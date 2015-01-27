46200f80ea3f522de373d688d777f54573d3be4b
Luke Cheng <luke@Lukes-MacBook-Pro.local>
2015-01-26 18:23:16 -0800

    Initial Commit

---
+++ CoinTossDiceRoll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoinTossDiceRoll
+//
+//  Created by Luke Cheng on 1/26/15.
+//  Copyright (c) 2015 cjmain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoinTossDiceRoll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoinTossDiceRoll
+//
+//  Created by Luke Cheng on 1/26/15.
+//  Copyright (c) 2015 cjmain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
